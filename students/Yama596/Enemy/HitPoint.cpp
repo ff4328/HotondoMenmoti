476cc0eef32d4cf1800dc829cf415d73f1edda53
Yama596 <csgesu42bro@outlook.jp>
2026-04-16 12:47:47 +0900

    確認

---
+++ students/Yama596/Enemy/HitPoint.cpp
@@ -0,0 +1,79 @@
+#include "../students/Yama596/Enemy/HitPoint.h"
+#include "DxLib.h"
+
+HitPoint::HitPoint():
+	m_posX(),
+	m_posY(),
+	m_gaugePosX(),
+	m_gaugePosY(),
+	m_hp(0),
+	m_hpMax(0),
+	m_pPlayerMove(nullptr)
+{
+	
+}
+
+void HitPoint::Init()
+{
+
+}
+
+void HitPoint::Update()
+{
+
+}
+
+void HitPoint::Draw()
+{
+
+}
+
+void HitPoint::Finalize()
+{
+
+}
+
+void HitPoint::DebugShow()
+{
+	printfDx("åªç›ÇÃHP : %d\n", m_hp);
+}
+
+void HitPoint::Damage(int value)
+{
+
+	// à¯êîÇÃílÇhpÇ©ÇÁå∏éZ
+	m_hp -= value;
+
+	// 0à»â∫Ç…Ç»Ç¡ÇΩÇÁ0Ç…Ç∑ÇÈ
+	if (m_hp <= 0) m_hp = 0;
+
+}
+
+void HitPoint::Heal(int value)
+{
+
+}
+
+bool HitPoint::IsDead()
+{
+
+	// hpÇÃílÇ™0à»â∫Ç»ÇÁtrueÇï‘Ç∑
+	if (m_hp <= 0) return true;
+
+	// ÇªÇ§Ç∂Ç·Ç»ÇØÇÍÇŒfalseÇï‘Ç∑
+	return false;
+
+}
+
+void HitPoint::SetPosition(float _posX, float _posY)
+{
+
+}
+
+void HitPoint::SetHPMax(int value)
+{
+
+	m_hpMax = value;
+	m_hp = m_hpMax;
+
+}
