9cbf4ae36f19643c18a2fe90029ae1cefa3f39b3
oreistake <otakedazo16@icloud.com>
2026-04-16 15:01:05 +0900

    hitpoint

--- students/oreistake/HitPoint.cpp
+++ students/oreistake/HitPoint.cpp
@@ -39,16 +39,30 @@ void HitPoint::DebugShow()
 
 void HitPoint::Damage(int value)
 {
+	// 引数の値をhpから減算
+	m_hp -= value;
 
+	// 0以下になったら0にする
+	if (m_hp <= 0) m_hp = 0;
 }
 
 void HitPoint::Heal(int value)
 {
+	// 引数の値をhpから加算
+	m_hp += value;
 
+	// 最大HPを超えていないかチェック
+	if (m_hp > m_hpMax) m_hp = m_hpMax;
 }
 
 bool HitPoint::IsDead()
 {
+	// hpの値が0以下ならtrueを返す
+	if (m_hp <= 0) return true;
+
+	// そうじゃなければfalseを返す
+	return false;
+
 	return false;
 }
 
@@ -59,5 +73,6 @@ void HitPoint::SetPosition(float _posX, float _posY)
 
 void HitPoint::SetHPMax(int value)
 {
-
+	m_hpMax = value;
+	m_hp = m_hpMax;
 }
