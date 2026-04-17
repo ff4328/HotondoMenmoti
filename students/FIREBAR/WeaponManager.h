64b21ad189ad5c33544437efc5949bea382830c8
Yama596 <csgesu42bro@outlook.jp>
2026-04-17 11:58:24 +0900

    Revert "意味のないゲッター"
    This reverts commit 5f955f290d62eeaa4dc79b66265f9296a78a0d41.

--- students/FIREBAR/WeaponManager.h
+++ students/FIREBAR/WeaponManager.h
@@ -24,7 +24,6 @@ struct Weapons
 	//武器の攻撃速度
 	int attackSpeed;
 };
-
 const int WEAPON_NUM=4;
 
 class WeaponManager
@@ -56,14 +55,6 @@ public:
 	/// 武器の初期ステータス設定
 	/// </summary>
 	void SetWeaponStatus();
-
-	/// <summary>
-	/// 何番目の武器のステータスを持ってくるか
-	/// </summary>
-	/// <param name="num">武器の番号
-	/// ０：弓、１：刀、２：斧、３：魔法</param>
-	/// <returns>対応する武器のステータス</returns>
-	Weapons GetWeaponStatus(int num) { return weapons[num]; }
 
 	/// <summary>
 	///	攻撃力増加
